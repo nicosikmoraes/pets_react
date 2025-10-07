5a85f0ef9220a51b1ad912e32695adea49aeebba
nicosikmoraes <nicolas.moraes.ns@gmail.com>
2025-10-07 19:05:54 -0300

    1

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Home from "@/components/Home";
+
+export default function index() {
+  return (
+    <ScreenWrapper>
+      <Home />
+    </ScreenWrapper>
+  );
+}
+
+const styles = StyleSheet.create({});
